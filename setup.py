0d2c94fe83504b95d2f367f42d0336c4819afcee
rootxdwt <65634206+rootxdwt@users.noreply.github.com>
2022-11-22 12:27:52 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='GSHS-api-wrapper-py',
+      version='0.1',
+      description='python wrapper for GSHS',
+      author='Eunchong Oh',
+      author_email='eunchong@ecdev.me',
+      url='https://github.com/rootxdwt/gshs-api-wrapper-py/',
+      packages=['GSHS-api-wrapper-py'],
+     )
